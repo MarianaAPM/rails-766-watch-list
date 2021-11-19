bf7ef43ed19da724ec03b891daa8ace1ae518e2e
Mariana Marinho <marianaapmarinho@gmail.com>
2021-11-19 10:39:13 -0300

    day one

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
   
-  resources :lists, only: [:show, :new, :create]
+  resources :lists, only: [:show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
